3565f917accab353d419f61c3a7ea7dcab6477f9
Yujin <127370593+JungYujin-gitHub@users.noreply.github.com>
2024-07-31 22:44:25 +0900

    Create 최대공약수와최소공배수2609.java

---
+++ 백준/최대공약수와최소공배수2609.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a<b) {
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		System.out.println(gcd(a,b));
+		System.out.println(lcm(a,b));
+		
+	}
+	
+	static int gcd(int a, int b) {
+		if(b==0) return a;
+		return gcd(b,a%b);
+	}
+	
+	static int lcm(int a, int b) {
+		return a*b / gcd(a,b);
+	}
+}
