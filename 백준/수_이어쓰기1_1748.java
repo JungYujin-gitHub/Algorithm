fd4ddd4946cb9486c3c38e99627bf2ca6f546c8a
Yujin <127370593+JungYujin-gitHub@users.noreply.github.com>
2024-08-08 22:58:47 +0900

    Create 수_이어쓰기1_1748.java

---
+++ 백준/수_이어쓰기1_1748.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int result = 0;
+		int cnt = 1;
+		int check = 10;
+		for(int i=1;i<=n;i++) {
+			if(i==check) {
+				cnt++;
+				check *= 10;
+			}
+			result += cnt;
+		}
+		System.out.println(result);
+	}
+}
